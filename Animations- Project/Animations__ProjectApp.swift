c67046cac7202acfb1941a872255304567ed2ad5
Vishnu <vishnu.akhil10@gmail.com>
2021-02-01 12:07:22 +1100

    Initial Commit

---
+++ Animations- Project/Animations__ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animations__ProjectApp.swift
+//  Animations- Project
+//
+//  Created by Vishnu Akhil Upparapalle on 1/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Animations__ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
